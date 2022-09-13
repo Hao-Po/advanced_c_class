ae92630e939af1427eff42d63a331b2f6d5737ed
hao-po <wayispiderik@gmail.com>
2022-09-13 15:48:10 +0800

    week01

---
+++ week01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    char str[100];
+    printf("Please input a character sequence: ");
+    scanf("%s", str);
+
+
+    printf("%s",str[0]);
+
+
+    return 0;
+}
