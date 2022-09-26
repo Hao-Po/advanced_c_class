8c649369ed17017cde165ae12a1fa945b0676d20
hao-po <wayispiderik@gmail.com>
2022-09-27 00:45:38 +0800

    week03

--- week03/week03.c
+++ week03/week03.c
@@ -7,7 +7,17 @@ typedef struct num_list{
 }tNumList;
 
 void bubble_sort(tNumList *list){
+    int temp;
 
+    for(int i = 0; i < list->counts - 1; i++){
+        for(int j = i+1; j < list->counts; j++){
+            if(*(list->p_list+i) > *(list->p_list+j)){
+                temp = *(list->p_list+i);
+                *(list->p_list+i) = *(list->p_list+j);
+                *(list->p_list+j) = temp;
+            }
+        }
+    }
 }
 
 void merge(tNumList *list1, tNumList *list2){
@@ -15,13 +25,38 @@ void merge(tNumList *list1, tNumList *list2){
 }
 
 int main(void){
-    tNumList *list = (tNumList *) malloc (sizeof(tNumList) * 2);
-    // tNumList *list1 = list[0];
-
-
+    tNumList list1, list2;
+    
     printf("Please enter how many numbers in list1: ");
-    scanf("%d", &(list1->counts));
-    printf("    Please input %d numbers: ", list1->counts);
+    scanf("%d", &(list1.counts));
+
+    list1.p_list = (int*) malloc (sizeof(int) * list1.counts);
+
+    printf("    Please input %d numbers: ", list1.counts);
+    for(int index = 0; index < list1.counts; index++){
+        scanf("%d", list1.p_list + index);
+    }
+
+    // printf("Please enter how many numbers in list2: ");
+    // scanf("%d", &(list2.counts));
+
+    // list2.p_list = (int*) malloc (sizeof(int) * list2.counts);
+    
+    // printf("    Please input %d numbers: ", list2.counts);
+    // for(int index = 0; index < list2.counts; index++){
+    //     scanf("%d", list2.p_list + index);
+    // }
+
+    
+    bubble_sort(&list1);
+    printf("sorted list1: ");
+    for(int index = 0; index < list1.counts; index++){
+        printf("%d", *(list1.p_list + index));
+    }
+
+
+    
+    
 
     return 0;
 }
