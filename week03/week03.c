06c7f4457d6aa19a2b25d4f43dc1747c6b8fb5ad
Hao-Po <wayispiderik@gmail.com>
2022-09-26 21:29:50 +0800

    week03

--- week03/week03.c
+++ week03/week03.c
@@ -15,26 +15,13 @@ void merge(tNumList *list1, tNumList *list2){
 }
 
 int main(void){
-    tNumList *list1, *list2;
-    
+    tNumList *list = (tNumList *) malloc (sizeof(tNumList) * 2);
+    // tNumList *list1 = list[0];
+
+
     printf("Please enter how many numbers in list1: ");
     scanf("%d", &(list1->counts));
-
-    // int *list1_array = (int* )malloc(sizeof(int) * list1->counts);
-    // list1->p_list =  list1_array;
     printf("    Please input %d numbers: ", list1->counts);
-
-    for(int i = 0; i < 3; i++){
-        printf("!");
-    }
- 
-    
-    // printf("Please enter how many numbers in list2: ");
-    // scanf("%d", &(list2->counts));
-    
-    // int *list2_array = (int* )malloc(sizeof(int) * list2->counts);
-    // list2->p_list = list2_array;
-    // printf("    Please input %d numbers: ", list2->counts);
 
     return 0;
 }
