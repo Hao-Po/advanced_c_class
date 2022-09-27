f7cd0bf37f5bbd73123bafa42aa023ab86f24fea
Hao-Po <wayispiderik@gmail.com>
2022-09-27 16:22:32 +0800

    week03

--- week03/week03.c
+++ week03/week03.c
@@ -10,18 +10,50 @@ void bubble_sort(tNumList *list){
     int temp;
 
     for(int i = 0; i < list->counts - 1; i++){
-        for(int j = i+1; j < list->counts; j++){
-            if(*(list->p_list+i) > *(list->p_list+j)){
-                temp = *(list->p_list+i);
-                *(list->p_list+i) = *(list->p_list+j);
-                *(list->p_list+j) = temp;
+        for(int j = i + 1; j < list->counts; j++){
+            if(*(list->p_list + i) > *(list->p_list + j)){
+                temp = *(list->p_list + i);
+                *(list->p_list + i) = *(list->p_list + j);
+                *(list->p_list + j) = temp;
             }
         }
     }
 }
 
 void merge(tNumList *list1, tNumList *list2){
+    int list1_index = 0;
+    int list2_index = 0;
+    int merge_list_index = 0;
+    int *merge_list = (int* ) malloc (sizeof(int) * (list1->counts + list2->counts));
 
+    while((list1_index < list1->counts) && (list2_index < list2->counts)){
+        if (*(list1->p_list + list1_index) <= *(list2->p_list + list2_index)){
+            *(merge_list + merge_list_index) = *(list1->p_list + list1_index);
+            list1_index++;
+        }
+        else{
+            *(merge_list + merge_list_index) = *(list2->p_list + list2_index);
+            list2_index++;
+        }
+        merge_list_index++;
+    }
+
+    while(list1_index < list1->counts){
+        *(merge_list + merge_list_index) = *(list1->p_list + list1_index);
+        list1_index++;
+        merge_list_index++;
+    }
+
+    while(list2_index < list2->counts){
+        *(merge_list + merge_list_index) =*(list2->p_list + list2_index);
+        list2_index++;
+        merge_list_index++;
+    }
+
+    printf("merged list: ");
+    for(int index = 0; index < list1->counts + list2->counts; index++){
+        printf("%d ", *(merge_list + index));
+    }
 }
 
 int main(void){
@@ -37,26 +69,31 @@ int main(void){
         scanf("%d", list1.p_list + index);
     }
 
-    // printf("Please enter how many numbers in list2: ");
-    // scanf("%d", &(list2.counts));
+    printf("Please enter how many numbers in list2: ");
+    scanf("%d", &(list2.counts));
 
-    // list2.p_list = (int*) malloc (sizeof(int) * list2.counts);
+    list2.p_list = (int*) malloc (sizeof(int) * list2.counts);
     
-    // printf("    Please input %d numbers: ", list2.counts);
-    // for(int index = 0; index < list2.counts; index++){
-    //     scanf("%d", list2.p_list + index);
-    // }
-
+    printf("    Please input %d numbers: ", list2.counts);
+    for(int index = 0; index < list2.counts; index++){
+        scanf("%d", list2.p_list + index);
+    }
     
     bubble_sort(&list1);
+    bubble_sort(&list2);
+
     printf("sorted list1: ");
     for(int index = 0; index < list1.counts; index++){
-        printf("%d", *(list1.p_list + index));
+        printf("%d ", *(list1.p_list + index));
     }
+    printf("\n");
+    printf("sorted list2: ");
+    for(int index = 0; index < list2.counts; index++){
+        printf("%d ", *(list2.p_list + index));
+    }
+    printf("\n");
 
-
+    merge(&list1, &list2);
     
-    
-
     return 0;
 }
