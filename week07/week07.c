3b058e3ae8599abc0b6d0105c023b3780b495bd7
Hao-Po <wayispiderik@gmail.com>
2022-10-24 23:01:13 +0800

    week07

--- week07/week07.c
+++ week07/week07.c
@@ -68,7 +68,18 @@ void get_input(tNumStorHead *list){
             add_number(list);
         }
         else if (input == 2){
-            del_number(list);
+            if (list->counts != 0){
+                del_number(list);
+            }
+            else{
+                printf("  Nothing can delete.\n");
+            }
+        }
+        else if (input == -1){
+            break;
+        }
+        else{
+            printf("  No such choice !\n");
         }
     }
 }
@@ -85,33 +96,48 @@ void add_number(tNumStorHead *list){
     new_number->prev = NULL;
     new_number->next = NULL;
 
-    if (list->counts == 0){
+    if (list->counts == 0){         // the first insert
         list->head = new_number;
         list->tail = new_number; 
-    }else{
+    }
+    else{
         printf("  Specify a target location: ");
         scanf("%d", &location);
 
         node_ptr = list->head;
-        for(int i = 0; i < location - 1; i++){
+
+        for(int i = 0; i < location - 1; i++){  // go to location node
             node_ptr = node_ptr->next;
         }
 
         printf("  1. Before or 2. After the location %d: ", location);
         scanf("%d", &before_or_after);
 
-        if (before_or_after == 1){
-            if (node_ptr == list->head){
+        if (before_or_after == 1){                      // choose "before"
+            if (node_ptr == list->head){                // if location == head && insert "before" (insert head)
+                node_ptr->prev = new_number;            // ex. NULL <-- 3 <--> 5 --> NULL to NULL <-- 1 <--> 3 <--> 5 --> NULL
                 new_number->next = node_ptr;
                 list->head = new_number;
             }
-            else{
-                new_number->prev = node_ptr->prev;
+            else{                                       // other possibilities (insert before node)
+                new_number->prev = node_ptr->prev;      // ex. NULL <-- 3 <--> 5 --> NULL to NULL <-- 3 <--> 4 <--> 5 --> NULL
                 new_number->next = node_ptr;
+                node_ptr->prev->next = new_number;
+                node_ptr->prev = new_number;
             } 
         }
-        else if (before_or_after == 2){
-
+        else if (before_or_after == 2){                 // choose "after"
+            if (node_ptr == list->tail){                // if location == tail && insert "after" (insert tail)
+                node_ptr->next = new_number;            // ex. NULL <-- 3 <--> 5 --> NULL to NULL <-- 3 <--> 5 <--> 7 --> NULL
+                new_number->prev = node_ptr;
+                list->tail = new_number;
+            }
+            else{                                       // other possibilities (insert after node)
+                new_number->prev = node_ptr;            // ex. NULL <-- 3 <--> 5 --> NULL to NULL <-- 3 <--> 4 <--> 5 --> NULL
+                new_number->next = node_ptr->next;
+                node_ptr->next->prev = new_number;
+                node_ptr->next = new_number;
+            }
         }
     }
 
@@ -120,5 +146,38 @@ void add_number(tNumStorHead *list){
 }
 
 void del_number(tNumStorHead *list){
+    int location;
+    tNumStorage *node_ptr;
 
+    printf("  Specify a target location: ");
+    scanf("%d", &location);
+
+    if (list->counts == 1){    // only one node can delete.
+        list->head = NULL;
+        list->tail = NULL;
+    }
+    else{
+        node_ptr = list->head;
+
+        for(int i = 0; i < location - 1; i++){          // go to location node
+            node_ptr = node_ptr->next;
+        }
+
+        if (node_ptr == list->head){                    // del head node
+            list->head = node_ptr->next;
+            list->head->prev = NULL;
+        }
+        else if (node_ptr == list->tail){               // del tail node
+            list->tail = node_ptr->prev;
+            list->tail->next = NULL;
+        }
+        else{                                           // del middle node
+            node_ptr->next->prev = node_ptr->prev;
+            node_ptr->prev->next = node_ptr->next;
+        }
+    }
+
+    free(node_ptr);
+    list->counts--;
+    print_list(list);
 }
